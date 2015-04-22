fa625edbf39268d3f94d8c09d4e56539a036a181
kyletl <ktessierlavigne@gmail.com>
2015-04-22 18:23:42 -0400

    created new project

---
+++ 98 Project/98 Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  98 Project
+//
+//  Created by Kyle Tessier-Lavigne on 4/22/15.
+//  Copyright (c) 2015 Kyle Tessier-Lavigne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 98 Project/98 Project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  98 Project
+//
+//  Created by Kyle Tessier-Lavigne on 4/22/15.
+//  Copyright (c) 2015 Kyle Tessier-Lavigne. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
