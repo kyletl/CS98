f4b7afb9b9708a6441ea354985a73b5975b2d7aa
Taylor Cathcart <taylor.d.cathcart.15@dartmouth.edu>
2015-04-22 21:09:28 -0400

    added spotify tab controller

---
+++ 98 Project/SpotifyController.h
@@ -0,0 +1,13 @@
+//
+//  SpotifyController.h
+//  98 Project
+//
+//  Created by Taylor Cathcart on 4/22/15.
+//  Copyright (c) 2015 Kyle Tessier-Lavigne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpotifyController : UIViewController
+
+@end
