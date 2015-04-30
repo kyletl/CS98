ec7d7649e79a4c6f8fd899da843fb10cf4b1a6a1
kyletl <ktessierlavigne@gmail.com>
2015-04-29 20:25:31 -0400

    Added AudioController Files

---
+++ 98 Project/98 Project/AudioController.m
@@ -0,0 +1,87 @@
+//
+//  AudioController_AudioController.h
+//  98 Project
+//
+//  Created by Kyle Tessier-Lavigne on 4/29/15.
+//  Copyright (c) 2015 Kyle Tessier-Lavigne. All rights reserved.
+//
+
+
+
+@interface AudioController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *albumLabel;
+@property (weak, nonatomic) IBOutlet UILabel *artistLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *coverView;
+
+@property (nonatomic, strong) MPMusicPlayerController *musicPlayer;
+
+@end
+
+@implementation AudioController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.titleLabel.text = @"Nothing Playing";
+    self.albumLabel.text = @"";
+    self.artistLabel.text = @"";
+}
+
+-(IBAction)playPause:(id)sender {
+    if ([self.musicPlayer playbackState] == MPMusicPlaybackStatePaused) {
+        [self.musicPlayer play];
+    } else if ([self.musicPlayer playbackState] == MPMusicPlaybackStatePlaying) {
+        [self.musicPlayer pause];
+    }
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self handleNewSession];
+}
+
+- (void)handleNewSession {
+    
+    self.musicPlayer = [MPMusicPlayerController applicationMusicPlayer];
+    
+    [self.musicPlayer setQueueWithQuery: [MPMediaQuery songsQuery]];
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+    [notificationCenter
+     addObserver: self
+     selector:    @selector (handle_NowPlayingItemChanged:)
+     name:        MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+     object:      self.musicPlayer];
+    
+    [notificationCenter
+     addObserver: self
+     selector:    @selector (handle_PlaybackStateChanged:)
+     name:        MPMusicPlayerControllerPlaybackStateDidChangeNotification
+     object:      self.musicPlayer];
+    
+    [self.musicPlayer beginGeneratingPlaybackNotifications];
+    
+    [self.musicPlayer setShuffleMode: MPMusicShuffleModeOff];
+    [self.musicPlayer setRepeatMode: MPMusicRepeatModeNone];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
---
+++ 98 Project/AudioController.h
@@ -0,0 +1,15 @@
+//
+//  AudioController.h
+//  98 Project
+//
+//  Created by Kyle Tessier-Lavigne on 4/29/15.
+//  Copyright (c) 2015 Kyle Tessier-Lavigne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface AudioController : AVPlayerViewController
+
+@end
